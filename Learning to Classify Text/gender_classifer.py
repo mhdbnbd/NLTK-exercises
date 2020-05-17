c34166afec89488cf591de03e967ec04c2f52211
mhdbnbd <mehdi.benabed@live.fr>
2020-05-17 18:33:19 +0200

    names gender classifier 4/5 features

---
+++ Learning to Classify Text/gender_classifer.py
@@ -0,0 +1,43 @@
+import random
+import nltk
+from nltk.corpus import names
+from nltk.classify import apply_features
+
+# features extractor
+
+
+def a_features(word):
+    return {'first_letter': word[0]}
+
+
+def b_features(word):
+    return {'last_letter': word[-1]}
+
+
+def c_features(word):
+    return {'last_two_letters': word[-2:]}
+
+
+def d_features(word):
+    return {'word_length': len(word)}
+
+
+# labels for training/test
+labeled_names = ([(name, 'male') for name in names.words('male.txt')] +
+                 [(name, 'female') for name in names.words('female.txt')])
+
+random.shuffle(labeled_names)
+
+
+def train(features):
+    train_set = apply_features(features, labeled_names[500:])
+    test_set = apply_features(features, labeled_names[:500])
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
+    accuracy = nltk.classify.accuracy(classifier, test_set)
+    print(accuracy)
+    most_informative = classifier.show_most_informative_features(10)
+    return {accuracy, most_informative}
+
+
+for f in [a_features, b_features, c_features, d_features]:
+    train(f)
